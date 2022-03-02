ef46a8b6be8c7345ae411cdcd72e482e5fa213dc
Rafe Arnold <rafe.arnold@gmail.com>
2022-03-02 23:01:06 +0000

    Unblock DiscordBotRegister.

--- discord-bot/src/main/kotlin/uk/co/rafearnold/bincollection/discordbot/DiscordBotRegister.kt
+++ discord-bot/src/main/kotlin/uk/co/rafearnold/bincollection/discordbot/DiscordBotRegister.kt
@@ -5,16 +5,22 @@ import discord4j.core.GatewayDiscordClient
 import discord4j.core.`object`.entity.Member
 import discord4j.core.`object`.entity.Message
 import discord4j.core.event.domain.message.MessageCreateEvent
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
 import uk.co.rafearnold.bincollection.Register
 import uk.co.rafearnold.bincollection.discordbot.command.DiscordCommandHandler
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import javax.inject.Inject
 
 class DiscordBotRegister @Inject constructor(
     private val commandHandler: DiscordCommandHandler,
     private val appProps: Map<String, String>
 ) : Register {
+
+    private val connectionExecutor: Executor = Executors.newSingleThreadExecutor()
 
     override fun register(): CompletableFuture<Void> =
         CompletableFuture.runAsync {
@@ -34,6 +40,16 @@ class DiscordBotRegister @Inject constructor(
                             }
                     }
                 }
-            connection.block()
+            connectionExecutor.execute {
+                try {
+                    connection.block()
+                } catch (e: Throwable) {
+                    log.error("HTTP server failed", e)
+                }
+            }
         }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(DiscordBotRegister::class.java)
+    }
 }
